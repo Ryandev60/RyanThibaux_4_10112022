3da788aa0eae0eeee2b3edba84719543d34460c9
Ryan Thibaux <rthibaux@sogec-marketing.fr>
2022-11-11 17:13:50 +0100

    tri

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,175 +17,170 @@ const formData = document.querySelectorAll(".formData");
 
 // DOM form content
 
-const closeSubMenu = document.getElementById('closeSubMenu');
+const closeSubMenu = document.getElementById("closeSubMenu");
 
 // DOM form elements
-const formContent = document.getElementById('formContent');
-const formBody = document.getElementById('formBody');
-const form = document.querySelector('form');
-const firstNameInput = document.getElementById('first');
-const lastNameInput = document.getElementById('last');
-const emailInput = document.getElementById('email');
-const birthdateInput = document.getElementById('birthdate');
-const quantityInput = document.getElementById('quantity');
-const locInput1 = document.getElementById('location1');
-const locInput2 = document.getElementById('location2');
-const locInput3 = document.getElementById('location3');
-const locInput4 = document.getElementById('location4');
-const locInput5 = document.getElementById('location5');
-const locInput6 = document.getElementById('location6');
-const conditions = document.getElementById('checkbox1');
-const submitForm = document.getElementById('submitForm');
+const formContent = document.getElementById("formContent");
+const formBody = document.getElementById("formBody");
+const form = document.querySelector("form");
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const locInput1 = document.getElementById("location1");
+const locInput2 = document.getElementById("location2");
+const locInput3 = document.getElementById("location3");
+const locInput4 = document.getElementById("location4");
+const locInput5 = document.getElementById("location5");
+const locInput6 = document.getElementById("location6");
+const conditions = document.getElementById("checkbox1");
+const submitForm = document.getElementById("submitForm");
 
 // Err txt
 
-const firstNameErr = "Entrer 2 caractères ou plus pour le champ du prénom."
-const lastNameErr = "Entrer 2 caractères ou plus pour le champ du nom."
-const emailErr = "Entrer un email correct."
-const birthdateErr = "Entrer une date de naissance correct."
-const quantityErr = "Entrer un nombre de participation correct."
-const locErr = "Selectionner à quel tournoi vous avez participé cette année."
-const conditionsErr = "Accepter les conditions d'utilisation"
+const firstNameErr = "Entrer 2 caractères ou plus pour le champ du prénom.";
+const lastNameErr = "Entrer 2 caractères ou plus pour le champ du nom.";
+const emailErr = "Entrer un email correct.";
+const birthdateErr = "Entrer une date de naissance correct.";
+const quantityErr = "Entrer un nombre de participation correct.";
+const locErr = "Selectionner à quel tournoi vous avez participé cette année.";
+const conditionsErr = "Accepter les conditions d'utilisation";
 
 // Thx txt
 
-const txtThx = "Merci pour votre inscription"
+const txtThx = "Merci pour votre inscription";
+
+//RegExp
+
+const regExpEmail = new RegExp(
+  "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
+);
+
+const regExpNumber = new RegExp("^[0-99]");
+
+// Launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Close subscribe menu
+
+closeSubMenu.addEventListener("click", () => {
+  modalbg.style.display = "none";
+});
 
 // Function show error
 
 const showErr = (e, txtErr, id) => {
   if (!document.getElementById(id)) {
     const createTxtErr = document.createElement("p");
-    createTxtErr.classList.add('err-txt');
-    createTxtErr.id = id
+    createTxtErr.classList.add("err-txt");
+    createTxtErr.id = id;
     e.after(createTxtErr);
     createTxtErr.innerText = txtErr;
   }
-}
+};
 
 // Function remove error
 
 const removeErr = (id) => {
   document.getElementById(id) ? document.getElementById(id).remove() : null;
-}
-
-//RegExp
-
-const regExpEmail = new RegExp(
-  "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
-)
-
-const regExpNumber = new RegExp(
-  "^[0-99]"
-)
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-//Close subscribe menu
-
-closeSubMenu.addEventListener('click', () => {
-  modalbg.style.display = "none"
-})
+};
 
 //Test input form
 
-submitForm ? submitForm.addEventListener('click', (e) => {
+submitForm
+  ? submitForm.addEventListener("click", (e) => {
+      // Override the form's default behavior
 
-  // Override the form's default behavior
+      e.preventDefault();
 
-  e.preventDefault()
+      //Check firstName
 
-  //Check firstName
+      if (firstNameInput.value.length > 1) {
+        removeErr("txtErrFirstName");
+      } else {
+        showErr(firstNameInput, firstNameErr, "txtErrFirstName");
+      }
 
-  if (firstNameInput.value.length > 1) {
-    removeErr("txtErrFirstName")
-  } else {
-    showErr(firstNameInput, firstNameErr, "txtErrFirstName")
-  }
+      //Check lastName
 
-  //Check lastName
+      if (lastNameInput.value.length > 1) {
+        removeErr("txtErrLastName");
+      } else {
+        showErr(lastNameInput, lastNameErr, "txtErrLastName");
+      }
 
-  if (lastNameInput.value.length > 1) {
-    removeErr("txtErrLastName")
-  } else {
-    showErr(lastNameInput, lastNameErr, "txtErrLastName")
-  }
+      //Check email
 
-  //Check email
+      if (regExpEmail.test(emailInput.value)) {
+        removeErr("txtErrEmail");
+      } else {
+        showErr(emailInput, emailErr, "txtErrEmail");
+      }
 
-  if (regExpEmail.test(emailInput.value)) {
-    removeErr("txtErrEmail")
-  } else {
-    showErr(emailInput, emailErr, "txtErrEmail")
-  }
+      // Check birthdate
 
-  // Check birthdate
+      if (birthdateInput.value) {
+        removeErr("txtErrBirthdate");
+      } else {
+        showErr(birthdateInput, birthdateErr, "txtErrBirthdate");
+      }
 
-  if (birthdateInput.value) {
-    removeErr("txtErrBirthdate")
-  } else {
-    showErr(birthdateInput, birthdateErr, "txtErrBirthdate")
-  }
+      //Check quantity
 
+      if (regExpNumber.test(quantityInput.value)) {
+        removeErr("txtErrQuantity");
+      } else {
+        showErr(quantityInput, quantityErr, "txtErrQuantity");
+      }
 
-  //Check quantity
+      //Check location
 
-  if (regExpNumber.test(quantityInput.value)) {
-    removeErr("txtErrQuantity")
-  } else {
-    showErr(quantityInput, quantityErr, "txtErrQuantity")
+      if (
+        locInput1.checked ||
+        locInput2.checked ||
+        locInput3.checked ||
+        locInput4.checked ||
+        locInput5.checked ||
+        locInput6.checked
+      ) {
+        removeErr("txtErrLoc");
+      } else {
+        showErr(lastLoc, locErr, "txtErrLoc");
+      }
 
-  }
+      // Check conditons
 
-  //Check location
+      if (conditions.checked) {
+        removeErr("txtErrConditions");
+      } else {
+        showErr(checkConditions, conditionsErr, "txtErrConditions");
+      }
 
-  if (locInput1.checked || locInput2.checked || locInput3.checked || locInput4.checked || locInput5.checked || locInput6.checked) {
-    removeErr("txtErrLoc")
-  } else {
-    showErr(lastLoc, locErr, "txtErrLoc")
-  }
+      if (document.getElementsByClassName("err-txt").length === 0) {
+        // Create thanks text
+        formBody.remove();
+        const createTxtThx = document.createElement("p");
+        createTxtThx.classList.add("thx-txt");
+        formContent.appendChild(createTxtThx);
+        createTxtThx.innerText = txtThx;
 
-  // Check conditons
+        //Create close button
 
-  if (conditions.checked) {
-    removeErr("txtErrConditions")
-  } else {
-    showErr(checkConditions, conditionsErr, "txtErrConditions")
-  }
-
-
-
-  if (document.getElementsByClassName('err-txt').length === 0) {
-
-    // Create thanks text
-    formBody.remove();
-    const createTxtThx = document.createElement('p');
-    createTxtThx.classList.add('thx-txt');
-    formContent.appendChild(createTxtThx);
-    createTxtThx.innerText = txtThx;
-
-    //Create close button
-
-    const createCloseBtn = document.createElement('btn');
-    createCloseBtn.classList.add('close-btn');
-    formContent.appendChild(createCloseBtn);
-    createCloseBtn.innerText = 'Fermer';
-    createCloseBtn.addEventListener('click', () => {
-      modalbg.style.display = "none"
-
+        const createCloseBtn = document.createElement("btn");
+        createCloseBtn.classList.add("close-btn");
+        formContent.appendChild(createCloseBtn);
+        createCloseBtn.innerText = "Fermer";
+        createCloseBtn.addEventListener("click", () => {
+          modalbg.style.display = "none";
+        });
+      } else {
+        console.log("y'a une erreur");
+      }
     })
-
-  } else {
-    console.log("y'a une erreur");
-  }
-}
-) : null;
-
-
-
+  : null;
